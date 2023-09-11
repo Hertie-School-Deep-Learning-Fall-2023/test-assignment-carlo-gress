0a18c7e59828f0bc86168decd4e98f0474ef3285
carlo-gress <72525078+carlo-gress@users.noreply.github.com>
2023-09-11 09:27:13 +0200

    Update implement_me.py

--- implement_me.py
+++ implement_me.py
@@ -1,5 +1,14 @@
 
 def lcm(a, b):
-  """compute Least Common Multiple between two integers."""
+  if x > y:
+      z = x
+  else:
+      z = y
+  while(True):
+      if((z % x == 0) and (z % y == 0)):
+          lcm = z
+          break
+      z += 1
+  return lcm
 
   pass
